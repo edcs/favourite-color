f3bc0b4ace5213c39cf2cb3145b2231a9c7b9402
Edward Coleridge Smith <e.coleridgesmith@edcs.me>
2016-12-26 09:32:46 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import colors from './colors.json';
+import './App.css';
+
+class App extends Component {
+    listItems() {
+        let items = [];
+
+        for (let color of colors) {
+            items.push(
+                <li key={color.id} style={{ backgroundColor: color.color}} />
+            )
+        }
+
+        return items;
+    }
+
+    render() {
+        return (
+            <ul className="App">
+                {this.listItems()}
+            </ul>
+        );
+    }
+}
+
+export default App;
