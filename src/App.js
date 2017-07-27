41171ce663caf38a147b5d95c42491923b32e4dd
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2017-07-27 21:05:45 +0100

    Show number of stars and RTs

--- src/App.js
+++ src/App.js
@@ -3,25 +3,43 @@ import colors from '/tmp/colors.json';
 import './App.css';
 
 class App extends Component {
-    listItems() {
-        let items = [];
+  listItems() {
+    let items = [];
 
-        for (let color of colors) {
-            items.push(
-                <li key={color.id} style={{ backgroundColor: color.color}} />
-            )
-        }
-
-        return items;
+    for (let color of colors) {
+      items.push(
+        <li key={color.id} style={{ backgroundColor: color.color}}>
+          <a href={`https://twitter.com/everycolorbot/status/${color.id}`} target="_blank">
+            <span className="rts" title={`${color.retweets} Retweets`}>
+              💫 {color.retweets}
+              </span>
+              <span className="hearts" title={`${color.favourites} Likes`}>
+              💕 {color.favourites}
+            </span>
+          </a>
+        </li>
+      )
     }
 
-    render() {
-        return (
-            <ul className="App">
-                {this.listItems()}
-            </ul>
-        );
-    }
+    return items;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Favourite Color 🌈</h1>
+        <p>
+          This little app tries to decide what people's favourite colors are based on
+          likes and retweets from <a href="https://twitter.com/everycolorbot">@everycolorbot</a>.
+          To find out more, check out <a href="https://github.com/edcs/favourite-color">
+          github.com/edcs/favourite-color</a>
+        </p>
+        <ul className="App">
+          {this.listItems()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
